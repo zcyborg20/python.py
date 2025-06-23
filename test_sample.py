17df2546eea93d8c0a05b6a0e32edb9aac08cffc
Zeina Ahmed <104915189+zcyborg20@users.noreply.github.com>
2025-06-23 22:12:23 +0300

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,2 +1,24 @@
-def test_add():
-    assert 1 + 1 == 2
+python.py/
+├── Jenkinsfile
+├── requirements.txt   # optional
+└── tests/
+    ├── test_sample.py
+    ├── test_math.py
+    └── test_strings.py
+    
+test_sample.py
+    def test_basic():
+    assert 2 + 2 == 4
+
+    
+    test_math.py
+def test_subtract():
+    assert 5 - 3 == 2
+
+    
+
+    test_strings.py 
+def test_uppercase():
+    assert 'hello'.upper() == 'HELLO'
+
+    
